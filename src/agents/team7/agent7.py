3c02fca30c0c88f68b09072c5328639890b44f7e
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-03-25 09:46:56 +0100

    Completed the 2a exercice

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -11,7 +11,9 @@ class Agent7(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team7" # replace with your chosen name
+        self.name = "Greedharry-Dylan"
+        # On initialise une variable afin d'avoir les pas de temps
+        self.pas_temps = 0
 
     def reset(self):
         self.obs, _ = self.env.reset()
@@ -21,15 +23,22 @@ class Agent7(KartAgent):
         return self.isEnd
 
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
-        action = {
-            "acceleration": acceleration,
-            "steer": steering,
-            "brake": False, # bool(random.getrandbits(1)),
-            "drift": bool(random.getrandbits(1)),
-            "nitro": bool(random.getrandbits(1)),
-            "rescue":bool(random.getrandbits(1)),
-            "fire": bool(random.getrandbits(1)),
-        }
-        return action
+    	# On ajoute la condition qu'on avance le temps de 200 pas de temps
+    	if (self.pas_temps < 200):
+    		print(self.pas_temps) # On affiche les pas de temps
+    		acceleration = 1
+    		#On regarde le 3ème noeud afin d'avoir le steering
+    		steering = np.array(obs["paths_end"][self.path_lookahead-1][0])
+    		action = {
+    			"acceleration": acceleration,
+    			"steer": steering,
+    			# On met tout à False afin que ça ne perturbe pas le kart
+    			# avec des infos non nécessaire
+    			"brake": False,
+    			"drift": False,
+    			"nitro": False,
+    			"rescue": False,
+    			"fire": False,
+    		}
+    		self.pas_temps += 1 # Incrémentation de la variable pas de temps
+    		return action
