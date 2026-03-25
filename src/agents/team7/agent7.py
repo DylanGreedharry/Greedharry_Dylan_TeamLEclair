6db061ef467ffd3e8b102962a20528206c49f0ce
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-03-25 10:09:41 +0100

    Completed the first draft of the exercice 2b, might modify it a bit later if I still have time

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -14,6 +14,7 @@ class Agent7(KartAgent):
         self.name = "Greedharry-Dylan"
         # On initialise une variable afin d'avoir les pas de temps
         self.pas_temps = 0
+        self.marche_arr_time = 0
 
     def reset(self):
         self.obs, _ = self.env.reset()
@@ -23,6 +24,7 @@ class Agent7(KartAgent):
         return self.isEnd
 
     def choose_action(self, obs):
+    	#Exercice 2a
     	# On ajoute la condition qu'on avance le temps de 200 pas de temps
     	if (self.pas_temps < 200):
     		print(self.pas_temps) # On affiche les pas de temps
@@ -42,3 +44,36 @@ class Agent7(KartAgent):
     		}
     		self.pas_temps += 1 # Incrémentation de la variable pas de temps
     		return action
+    	# Exercice 2b
+    	# On ajoute la condition que le kart fait marche arrière le temps de 200 pas de temps
+    	elif (self.marche_arr_time < 200):
+    		print(self.marche_arr_time) # On affiche le timer de la marche arrière
+    		acceleration = 0
+    		brake = True
+    		#On regarde le 3ème noeud afin d'avoir le steering
+    		steering = -(np.array(obs["paths_end"][self.path_lookahead-1][0]))
+    		self.marche_arr_time += 1 # Incrémentation de la variable marche arrière
+    		return {
+    			"acceleration": acceleration,
+    			"steer": steering,
+				# On ajoute la variable brake
+				"brake": brake,
+				# On met tout à False afin que ça ne perturbe pas le kart
+				# avec des infos non nécessaire
+				"drift": False,
+				"nitro": False,
+				"rescue": False,
+				"fire": False,
+			}
+		#else: # Le kart ne bouge plus après 400 pas de temps
+		#	acceleration = 0
+		#	steering = 0
+		#	return {
+		#		"acceleration" : acceleration,
+		#		"steer": steering,
+		#		"brake": True,
+		#		"drift": False,
+		#		"nitro" : False,
+		#		"rescue" : False,
+		#		"fire" : False,
+		#	}
