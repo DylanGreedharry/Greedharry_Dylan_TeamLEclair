8b3b3f16fcac70185bad3cfbcd025cfed90f9868
Dylan <dylan.greedharry@etu.sorbonne-universite.fr>
2026-03-25 10:48:48 +0100

    Completed the exercice 3a imperfectly but no time left...

---
+++ src/agents/team1/agent1.py
@@ -0,0 +1,53 @@
+import numpy as np
+import random
+
+from utils.track_utils import compute_curvature, compute_slope
+from agents.kart_agent import KartAgent
+
+class Agent1(KartAgent):
+    def __init__(self, env, path_lookahead=3):
+        super().__init__(env)
+        self.path_lookahead = path_lookahead
+        self.agent_positions = []
+        self.obs = None
+        self.isEnd = False
+        self.name = "Greedharry-Dylan"
+        self.time_marche_arr_init = 0
+
+    def reset(self):
+        self.obs, _ = self.env.reset()
+        self.agent_positions = []
+
+    def endOfTrack(self):
+        return self.isEnd
+
+    def choose_action(self, obs):
+        if (self.time_marche_arr_init < 60):
+            acceleration = 0.1
+            steering = 1
+            action = {
+                "acceleration": acceleration,
+                "steer": steering,
+                "brake": False, # bool(random.getrandbits(1)),
+                "drift": bool(random.getrandbits(1)),
+                "nitro": bool(random.getrandbits(1)),
+                "rescue":bool(random.getrandbits(1)),
+                "fire": bool(random.getrandbits(1)),
+            }
+            self.time_marche_arr_init += 1
+            print(self.time_marche_arr_init)
+            return action
+        else:
+            acceleration = 0
+            steering = obs["paths_end"][self.path_lookahead-1][0]
+            action = {
+                "acceleration": acceleration,
+                "steer": steering,
+                "brake": True, # bool(random.getrandbits(1)),
+                "drift": bool(random.getrandbits(1)),
+                "nitro": bool(random.getrandbits(1)),
+                "rescue":bool(random.getrandbits(1)),
+                "fire": bool(random.getrandbits(1)),
+            }
+            self.time_marche_arr_init += 1
+            return action
